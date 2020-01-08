dbf14e1b8ce0f67ee8ef38860ed796c470e9f56b
kuroiss <53767318+kuroiss@users.noreply.github.com>
2020-01-08 11:02:38 +0900

    Add files via upload

--- display_graph.py
+++ display_graph.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os, inspect
 import tkinter as tk
-from tkinter import *
 import tkinter.ttk as ttk
 
 ######################################################################
